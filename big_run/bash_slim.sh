6cebf4408f4ce9ad9e9ef06fef176b0f199bfbce
TatianaBellagio <tatianabellagio@gmail.com>
2023-06-17 11:37:42 -0600

    clean directory

--- big_run/bash_slim.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Basic for loop
-optimas=(0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33)
-for optima_value in "${optimas[@]}"
-do
-slim -d optima=$optima_value -d replicates=12 -d gen_to_run=3 arabidopsis_evolve.slim
-
-done
-echo simulation done
--- snakemake/scripts/apply_ld_pruning.sh
+++
@@ -1,6 +0,0 @@
-
-allele_counts_table="${snakemake_input[allele_counts_table]}"
-prune_in="${snakemake_input[snps_to_keep]}"
-allele_counts_table_filtered="${snakemake_output[allele_counts_table_filtered]}"
-
-awk -F ',' 'BEGIN{OFS=","} NR==FNR{values[substr($1, 3)]; next} {gsub(/^5_/, "", $1); if ($1 in values) print}' "${prune_in}" "${allele_counts_table}" > "${allele_counts_table_filtered}" 2> "${snakemake_log[0]}"

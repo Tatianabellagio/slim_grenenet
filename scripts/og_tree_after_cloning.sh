513e3082f54a2bf3c55a52342f950bf51569ee65
TatianaBellagio <tatianabellagio@gmail.com>
2023-09-13 09:43:25 -0700

    change slim slone

--- scripts/og_tree_after_cloning.sh
+++ scripts/og_tree_after_cloning.sh
@@ -1,14 +1,3 @@
-# Paths to the input text files
-tree_file="data/og_tree_offset.trees"
-## output
-output_file="data/og_tree_offset_after_cloning.trees"
-
-echo $tree_file
-echo $output_file
-
-slim \
-    -d "tree='$tree_file'" \
-    -d "output='$output_file'" \
-    scripts/tree_after_cloning.slim
+slim scripts/tree_after_cloning.slim
 
 

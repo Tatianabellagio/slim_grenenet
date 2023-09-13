0ff68036cb44af36db39caee80686a8655d7d6f3
TatianaBellagio <tatianabellagio@gmail.com>
2023-09-13 09:37:22 -0700

    troubleshott slim cloning

--- scripts/og_tree_after_cloning.sh
+++ scripts/og_tree_after_cloning.sh
@@ -3,9 +3,13 @@ tree_file="data/og_tree_offset.trees"
 ## output
 output="data/og_tree_offset_after_cloning.trees"
 
+echo tree_file
+echo output
+
+
 slim \
     -d "tree='$tree_file'" \
     -d "output='$output'" \
-    scripts/tree_after_cloning.slim 
+    scripts/tree_after_cloning.slim
 
-echo "made it here?"
+

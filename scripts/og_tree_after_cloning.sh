6a2cef90d45a35adf6107dd58866e86f957508b1
TatianaBellagio <tatianabellagio@gmail.com>
2023-09-13 09:41:24 -0700

    mm

--- scripts/og_tree_after_cloning.sh
+++ scripts/og_tree_after_cloning.sh
@@ -1,15 +1,14 @@
 # Paths to the input text files
 tree_file="data/og_tree_offset.trees"
 ## output
-output="data/og_tree_offset_after_cloning.trees"
+output_file="data/og_tree_offset_after_cloning.trees"
 
 echo $tree_file
-echo $output
-
+echo $output_file
 
 slim \
     -d "tree='$tree_file'" \
-    -d "output='$output'" \
+    -d "output='$output_file'" \
     scripts/tree_after_cloning.slim
 
 

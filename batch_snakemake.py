edf1506c1c3a229854ecc772b578e59dd893b310
TATUSA <tatianabellagio@gmail.com>
2023-10-16 20:49:17 -0700

    ignore all data

--- batch_snakemake.py
+++ batch_snakemake.py
@@ -1,9 +1,9 @@
 import subprocess
 
 # List of batch sizes to run
-batch_sizes = [1, 2, 3, 4, 5 , 6, 7, 8, 9, 10 ,11,12,13,14,15,16,17,18,19,20]
+batch_sizes = [1,2,3,4,5,6, 7,8,9,10]
 
 # Loop through batch sizes
 for batch_size in batch_sizes:
-    command = f"snakemake --rerun-triggers mtime --profile profiles/slurm/ --latency-wait 20 --batch all={batch_size}/20"
+    command = f"snakemake --rerun-triggers mtime --profile profiles/slurm/ --latency-wait 20 --batch all={batch_size}/10"
     subprocess.run(command, shell=True)

37aa0092cf2def3e23c604009e041601bc8480dd
Tatiana <tatiana@dpbdgeloaner1.local>
2023-10-19 15:53:29 -0700

    check geno fam

--- scripts/gwas_gemma.sh
+++ scripts/gwas_gemma.sh
@@ -7,7 +7,7 @@ output="${snakemake_output[output_gwas]}"
 ## extract the directory in which the fam file is 
 fam_dir=$(dirname "$fam_file")
 
-#move to that directory 
+#move to that directoryd 
 cd "$fam_dir"
 echo "$fam_dir"
 ## create the hardlinks to bed bim and kinship matrix
@@ -18,9 +18,11 @@ ln -sf "../../../../../../$bim_file" geno.bim
 ln -sf "../../../../../../$bed_file" geno.bed
 
 ## adn now that all the files are in the same folder, run gemma 
+awk 'BEGIN { rows=0; cols=0; } { rows++; cols=NF; } END { print rows, cols; }' geno.fam
 
-## Check if all individuals have the phenotype value as 0
-if awk -F ' ' '{print $6}' "geno.fam"| grep -vqE '^0(\.0+)?$'; then
+
+#if awk -F ' ' 'NF==6 {exit 1}' "../data/greneNet_final_v1.1.recode.fam"; then
+if awk -F ' ' 'NF!=6 {flag=1; exit} END {if (flag) exit 1}' "geno.fam"; then
     ## Run GWAS with Gemma gemma -bfile geno -lmm -k kinship.cXX.txt -o results -maf 0.00001
     gemma -bfile geno -lmm -k kinship.cXX.txt -o results -maf 0.00001
 else
@@ -28,4 +30,3 @@ else
     mkdir output
     echo "All individuals died" > "output/results.assoc.txt"
 fi
- 
--- scripts/test_gwa.sh
+++ scripts/test_gwa.sh
@@ -1,10 +1,17 @@
-## Check if all individuals have the phenotype value as 0
-if awk -F ' ' '{print $6}' "../data/greneNet_final_v1.1.recode.fam"| grep -vqE '^0(\.0+)?$'; then
-#if awk -F ' ' '{print $6}' "../data/greneNet_final_v1.1.recode.fam" | grep -qE -v '^0(\.0+)?$'; then
+
+echo awk 'BEGIN { rows=0; cols=0; } { rows++; cols=NF; } END { print rows, cols; }' ../data/greneNet_final_v1.1.recode.fam
+
+#if awk -F ' ' 'NF==6 {exit 1}' "../data/greneNet_final_v1.1.recode.fam"; then
+if awk -F ' ' 'NF!=6 {flag=1; exit} END {if (flag) exit 1}' "../data/greneNet_final_v1.1.recode.fam"; then
+
     ## Run GWAS with Gemma gemma -bfile geno -lmm -k kinship.cXX.txt -o results -maf 0.00001
     echo run_gemma
 else
     ## Create a file indicating all individuals died
     echo dont run gemma 
 fi
- 
+
+
+
+#if awk -F ' ' '{print $6}' "geno.fam"| grep -vqE '^0(\.0+)?$'; then
+    

25fb9fb6d2fa06ab42f4561f5b88a38b1f6ea286
TATUSA <tatianabellagio@gmail.com>
2023-11-19 00:44:38 -0800

    little change in batch

--- batch_snakemake.py
+++ batch_snakemake.py
@@ -5,5 +5,5 @@ batch_sizes = [1,2,3,4]
 
 # Loop through batch sizes
 for batch_size in batch_sizes:
-    command = f"snakemake --rerun-triggers mtime --use-conda --conda-frontend mamba --profile profiles/slurm/ --latency-wait 20 --batch all={batch_size}/4"
+    command = f"snakemake --rerun-triggers mtime --use-conda --conda-frontend mamba --profile profiles/slurm/ --scheduler greedy --batch all={batch_size}/4"
     subprocess.run(command, shell=True)

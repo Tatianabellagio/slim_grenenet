1e6302795538c695948b7b72e434d96c4cdc0962
Tatiana <tatiana@dpbdgeloaner1.local>
2023-11-20 21:30:41 -0800

    changememoru

--- scripts/create_pheno_hapfm.py
+++ scripts/create_pheno_hapfm.py
@@ -3,8 +3,6 @@ import pandas as pd
 fam_file = snakemake.input['fam_gwas'] 
 pheno_hapfm = snakemake.output['pheno_hapfm'] 
 
-pd.read_csv(fam_gwas,sep=' ')
-
-geno_fam = pd.read_csv('geno.fam',sep=' ',header=None)[5]
+geno_fam = pd.read_csv(fam_file,sep=' ',header=None)[5]
 geno_fam = geno_fam * 1000
 geno_fam.to_csv(pheno_hapfm,header=None, index=None)
